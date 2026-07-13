6a4d350c7945caf9c1c2c4c68ad7d62b367f2a32
Palz <palomamavacala@hotmail.com>
2026-07-13 09:52:42 +0100

    Create 01_create_database.sql
    Create enterprise risk database

---
+++ sql/01_create_database.sql
@@ -0,0 +1,14 @@
+/*
+=========================================================
+Enterprise Risk Register SQL Analysis
+File: 01_create_database.sql
+
+Objective:
+Create a MySQL database for analysing enterprise risks,
+controls, ownership and review activity.
+=========================================================
+*/
+
+CREATE DATABASE enterprise_risk_analysis;
+
+USE enterprise_risk_analysis;
